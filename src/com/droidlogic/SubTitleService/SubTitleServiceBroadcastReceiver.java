22f7ae967c343b3df2b9103a610df2c8b396c22c
xiaoliang.wang <xiaoliang.wang@amlogic.com>
2017-11-02 14:36:45 +0800

    Subtitle: add property to disable subtitle default[3/3]
    PD#153581
    
    add property to disable subtitle default
    
    Change-Id: Ifb5226bd4f519d7d8ab1b06a16afb15f3053e820

--- src/com/droidlogic/SubTitleService/SubTitleServiceBroadcastReceiver.java
+++ src/com/droidlogic/SubTitleService/SubTitleServiceBroadcastReceiver.java
@@ -5,15 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.os.ServiceManager;
+import com.droidlogic.app.SystemControlManager;
 
 public class SubTitleServiceBroadcastReceiver extends BroadcastReceiver {
         private static final String TAG = "SubTitleServiceBroadcastReceiver";
         private SubTitleService subtitleService = null;
+        private SystemControlManager mSystemControl;
 
         @Override
         public void onReceive (Context context, Intent intent) {
             String action = intent.getAction();
             Log.i (TAG, "[onReceive]action:" + action + ", subtitleService:" + subtitleService);
+
+            mSystemControl = new SystemControlManager(context);
+            if (mSystemControl.getPropertyBoolean("sys.subtitle.disable", true)) {
+                return;
+            }
+
             if (Intent.ACTION_BOOT_COMPLETED.equals (action)) {
                 if (ServiceManager.getService("subtitle_service") == null) {
                     subtitleService = new SubTitleService (context);
