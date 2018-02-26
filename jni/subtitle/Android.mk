be13c7184641df14eee65cf7a754f15dd6e56daf
Xindong Xu <xindong.xu@amlogic.com>
2018-02-26 10:09:30 +0800

    makefile : add / after BOARD_AML_VENDOR_PATH [1/4]
    PD# NONE
    
    add / after BOARD_AML_VENDOR_PATH
    
    Change-Id: I7e0d656150ff00a21fd0a424ed9b6e18a3b2ee0c

--- jni/subtitle/Android.mk
+++ jni/subtitle/Android.mk
@@ -7,14 +7,14 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := sub_jni.c sub_api.c log_print.c sub_subtitle.c sub_vob_sub.c sub_set_sys.c vob_sub.c sub_pgs_sub.c sub_control.c avi_sub.c sub_dvb_sub.c amsysfsutils.c Amsyswrite.cpp MemoryLeakTrackUtilTmp.cpp sub_socket.cpp sub_io.cpp
 LOCAL_ARM_MODE := arm
 LOCAL_C_INCLUDES := $(JNI_H_INCLUDE) \
-    $(TOP)/frameworks/native/services \
-    $(TOP)/frameworks/native/include
+    frameworks/native/services \
+    frameworks/native/include
 
 LOCAL_SHARED_LIBRARIES += libutils libmedia libcutils libbinder libsystemcontrolservice liblog vendor.amlogic.hardware.systemcontrol@1.0_vendor
 
 LOCAL_C_INCLUDES += \
-  $(BOARD_AML_VENDOR_PATH)frameworks/services/systemcontrol \
-  $(BOARD_AML_VENDOR_PATH)frameworks/services/systemcontrol/PQ/include
+  $(BOARD_AML_VENDOR_PATH)/frameworks/services/systemcontrol \
+  $(BOARD_AML_VENDOR_PATH)/frameworks/services/systemcontrol/PQ/include
 
 LOCAL_PRELINK_MODULE := false
 
