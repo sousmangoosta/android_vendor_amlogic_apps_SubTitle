7c05accb512e972578bb8ecbf9ad2f9c2085ad69
Lu Wang <lu.wang@amlogic.com>
2018-11-15 17:59:42 +0800

    subtitle: add amlogic license [2/2]
    PD#SWPL-2111
    
    Problem:
    no license
    
    Solution:
    add amlogic license
    
    Verify:
    compile success,no function
    
    Change-Id: I227c5d60b181b29b82e6c209798c42f6a8c660d9
    Signed-off-by: Lu Wang <lu.wang@amlogic.com>

--- jni/subtitle/sub_set_sys.h
+++ jni/subtitle/sub_set_sys.h
@@ -1,3 +1,11 @@
+/*
+* Copyright (c) 2014 Amlogic, Inc. All rights reserved.
+*
+* This source code is subject to the terms and conditions defined in the
+* file 'LICENSE' which is part of this source code package.
+*
+* Description:
+*/
 #ifndef _SUBTITLE_SET_DISP_H_
 #define _SUBTITLE_SET_DISP_H_
 
